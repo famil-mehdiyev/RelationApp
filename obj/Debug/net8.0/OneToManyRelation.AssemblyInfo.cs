cc42a496ce08fdce49d597a6ba768dccb79d075c
famil-mehdiyev <mehdiyev.fam@gmail.com>
2024-12-03 11:10:36 +0400

    yml file added

--- obj/Debug/net8.0/OneToManyRelation.AssemblyInfo.cs
+++ obj/Debug/net8.0/OneToManyRelation.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneToManyRelation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9be4f5d83219abaf8f15e322afdc9e5ac4bcf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3ccea522d54fe7c751f3eb5b1e3ea151196843")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneToManyRelation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneToManyRelation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
